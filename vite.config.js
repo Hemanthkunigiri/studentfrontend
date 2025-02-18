fa1b916d4c9e854a988197f7dab59a37b2a6d207
hemanth1409 <141008557+Hemanthkunigiri@users.noreply.github.com>
2025-02-18 13:55:37 +0530

    Updated feature or bug fixs description

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173, // Use Render's assigned port or default to 5173
+    host: "0.0.0.0", // Make the server accessible externally
+  },
   plugins: [
     tailwindcss(),
     react()
